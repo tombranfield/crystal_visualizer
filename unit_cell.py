4e17d98be6196891faf37c1aa3c3601c245ee586
Thomas Branfield <tbranfield@gmail.com>
2022-09-26 22:40:20 +0100

    Created unit_cell.py

---
+++ unit_cell.py
@@ -0,0 +1,12 @@
+# A class representing a unit cell of the crystal structure
+# It has the crystal system, the space group, the setting, and the lattice
+# parameters (basis vectors a, b, c, and angles alpha, beta, gamma)
+
+
+class UnitCell:
+    """
+    A class representing the unit cell of a crystal structure
+    """
+    pass
+
+
