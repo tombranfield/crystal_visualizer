ec5d6cd1b6c306085027be51dd73996b52bfad9e
Thomas Branfield <tbranfield@gmail.com>
2022-09-26 22:29:09 +0100

    Created file_reader.py

---
+++ file_reader.py
@@ -0,0 +1,12 @@
+# A module for reading input files
+
+# Reads an input file ending in .in
+
+# Gets atomic species, their fractional coordinates, and the basis vectors
+
+
+class FileReader:
+    """
+    A class for reading .in files containing crystal structures
+    """
+    pass
