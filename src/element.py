4a34cbb74a48e68a368d2e574a454fc671cd4fb2
Thomas Branfield <tbranfield@gmail.com>
2022-09-27 21:31:49 +0100

    Entered element info for periods 1-3 in element.py

--- src/element.py
+++ src/element.py
@@ -1,7 +1,37 @@
 from collections import namedtuple
 from typing import Union
 
+# Put element properties into a named tuple
+Element = namedtuple("Element", ["atomic_number", "symbol", "name"])
+
+# Period 1
+H = Element(1, "H", "hydrogen")
+He = Element(2, "He", "helium")
+
+# Period 2
+Li = Element(3, "Li", "lithium")
+Be = Element(4, "Be", "beryllium")
+B = Element(5, "B", "boron")
+C = Element(6, "C", "carbon")
+N = Element(7, "N", "nitrogen")
+O = Element(8, "O", "oxygen")
+F = Element(9, "F", "fluorine")
+Ne = Element(10, "Ne", "neon")
+
+# Period 3
+Na = Element(11, "Na", "sodium")
+Mg = Element(12, "Mg", "magnesium")
+Al = Element(13, "Al", "aluminium")
+Si = Element(14, "Si", "silicon")
+P = Element(15, "P", "phosphorus")
+S = Element(16, "S", "sulphur")
+Cl = Element(17, "Cl", "chlorine")
+Ar = Element(18, "Ar", "argon")
+
+# A tuple containing the element symbols in ascending atomic number
+# The tuple index corresponds to the atomic number
 ELEMENTS = (
+    None,
     H, He,
     Li, Be, B, C, N, O, F, Ne,
     Na, Mg, Al, Si, P, S, Cl, Ar,
@@ -10,18 +40,6 @@ ELEMENTS = (
     Cs, Ba, La, Ce, Pr, Nd, Pm, Sm, Eu, Gd, Tb, Dy, Ho, Er, Tm, Yb, Lu, Hf, Ta, W, Re, Os, Ir, Pt, Au, Hg, Tl, Pb, Bi, Po, At, Rn,
     Fr, Ra, Ac, Th, Pa, U, Np, Pu, Am, Cm, Bk, Cf, Es, Fm, Md, No, Lr, Rf, Db, Sg, Bh, Hs, Mt, Ds, Rg, Cn, Nh, Fl, Mc, Lv, Ts, Og
 )
-
-
-
-# Put element properties into a named tuple
-Element = namedtuple("Element", ["atomic_number", "symbol", "name"])
-
-H = Element(1, "H", "hydrogen")
-He = Element(2, "He", "helium")
-
-Li = Element(3, "Li", "lithium")
-
-
 
 class Element:
     """A class representing a chemical element."""
