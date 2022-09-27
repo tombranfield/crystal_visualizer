b47244bf3f7527a5d7f169e2f87ccb739205e315
Thomas Branfield <tbranfield@gmail.com>
2022-09-27 21:17:01 +0100

    Added element list and nametuple in element.py

--- src/element.py
+++ src/element.py
@@ -1,20 +1,34 @@
-# So what we want is to define
-# Element("Mg") or Element(123) or Element("Magnesium")
-# ie how to use multiple constructors in Python?
-# Depending on supplied argument, uses the appropriate constructor
-# Then constructs element object by referring to the table of constants
-# containing the Element information
-# So two issues
-# How to write multiple constructors
-# Where and how to write the table of chemical element information
+from collections import namedtuple
+from typing import Union
+
+ELEMENTS = (
+    H, He,
+    Li, Be, B, C, N, O, F, Ne,
+    Na, Mg, Al, Si, P, S, Cl, Ar,
+    K, Ca, Sc, Ti, V, Cr, Mn, Fe, Co, Ni, Cu, Zn, Ga, Ge, As, Se, Br, Kr,
+    Rb, Sr, Y, Zr, Nb, Mo, Tc, Ru, Rh, Pd, Ag, Cd, In, Sn, Sb, Te, I, Xe,
+    Cs, Ba, La, Ce, Pr, Nd, Pm, Sm, Eu, Gd, Tb, Dy, Ho, Er, Tm, Yb, Lu, Hf, Ta, W, Re, Os, Ir, Pt, Au, Hg, Tl, Pb, Bi, Po, At, Rn,
+    Fr, Ra, Ac, Th, Pa, U, Np, Pu, Am, Cm, Bk, Cf, Es, Fm, Md, No, Lr, Rf, Db, Sg, Bh, Hs, Mt, Ds, Rg, Cn, Nh, Fl, Mc, Lv, Ts, Og
+)
 
 
-from enum import Enum
 
-class Element(Enum):
-    """
-    A class representing a chemical element.
+# Put element properties into a named tuple
+Element = namedtuple("Element", ["atomic_number", "symbol", "name"])
 
-    It consists of a symbol, a name, and an atomic number.
-    """
-    pass
+H = Element(1, "H", "hydrogen")
+He = Element(2, "He", "helium")
+
+Li = Element(3, "Li", "lithium")
+
+
+
+class Element:
+    """A class representing a chemical element."""
+    def __init__(self, in_param: Union[str, int]):
+        """Initialize the element using its symbol, name, or atomic number"""
+        if isinstance(in_param, str):
+            if len(str) == 1 or len(str) == 2:
+                # Create an element with the symbol
+            else:
+                # Create an element with its name
