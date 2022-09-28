cad4cb86378096f4aca1fcaf5830eebae281c016
Thomas Branfield <tbranfield@gmail.com>
2022-09-28 23:42:30 +0100

    Wrote init constructor for Atom class

--- src/atom.py
+++ src/atom.py
@@ -14,8 +14,18 @@ class Atom:
                         in_fract_x: float,
                         in_fract_y: float,
                         in_fract_z: float):
-        """Initializes the Atom class."""
+        """Initializes the Atom class.
+
+        Note the chemical element of the atom is read-only, but the fractional
+        coordinates and label must be able to change.
+        """
         self._element = Element(in_chemical_symbol)
-        self._fract_x = in_fract_x
-        self._fract_y = in_fract_y
-        self._fract_z = in_fract_z
+        self.label = self._element.symbol
+        self.fract_x = in_fract_x
+        self.fract_y = in_fract_y
+        self.fract_z = in_fract_z
+
+    @property
+    def element(self) -> Element:
+        """Returns the chemical element of atom."""
+        return self._element
