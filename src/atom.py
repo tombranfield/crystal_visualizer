be5e3dca45623d271ac85925df874640ecfcf36d
Thomas Branfield <tbranfield@gmail.com>
2022-09-30 22:40:12 +0100

    Added property methods for y/z axis in atom.py

--- src/atom.py
+++ src/atom.py
@@ -44,18 +44,59 @@ class Atom:
     @property
     def element(self) -> Element:
         """Returns the chemical element of atom."""
-        return self._element
+        return self._element.symbol
 
     @property
     def fract_x(self):
-        """Returns the fractional coordinate on the x-axis of the atom"""
+        """Returns the x-axis fractional coordinate"""
         return self._fract_x
 
     @fract_x.setter
     def fract_x(self, in_fract_x):
         """Verifies and sets the value of the x-axis fractional coordinate"""
-        if not isinstance(in_fract_x, float):
-            raise AttributeError("Fractional coordinates must be floating numbers")
-        if in_fract_x < 0 or in_fract_x >= 1:
-            raise ValueError("Fractional coordinates must be in the range [0, 1)")
+        self.__verify_coordinate(in_fract_x)
         self._fract_x = in_fract_x
+
+    @property
+    def fract_y(self):
+        """Returns the y-axis fractional coordinate"""
+        return self._fract_y
+
+    @fract_y.setter
+    def fract_y(self, in_fract_y):
+        """Verifies and sets the value of the y-axis fractional coordinate"""
+        self.__verify_coordinate(in_fract_y)
+        self._fract_y = in_fract_y
+
+    @property
+    def fract_z(self):
+        """Returns the z-axis fractional coordinate"""
+        return self._fract_z
+
+    @fract_z.setter
+    def fract_z(self, in_fract_z):
+        "Verifies and sets the value of the z-axis fractional coordinate"
+        self.__verify_coordinate(in_fract_z)
+        self._fract_z = in_fract_z
+
+
+
+    def __verify_coordinate(self, in_fract_cood: float):
+        """Verifies that a given fractional coordinate is valid.
+
+        Raises an error if invalid, otherwise does nothing.
+        """
+        if not isinstance(in_fract_cood, float):
+            raise AttributeError("Fractional coordinates must be floating numbers")
+        if in_fract_cood < 0 or in_fract_cood >= 1:
+            raise ValueError("Fractional coordinates must be in the range [0, 1)")
+
+
+def main():
+    """Main method for this module, used primarily for quick testing"""
+    my_atom = Atom("Ba", 0.0, 0.5, 0.5)
+    print(my_atom.element)
+
+
+if __name__ == "__main__":
+    main()
