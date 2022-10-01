e22ed5832f201d4462ba494d8684e685c9b08670
Thomas Branfield <tbranfield@gmail.com>
2022-10-01 22:58:34 +0100

    Created lattice.py, wrote two docstrings in it

---
+++ src/lattice.py
@@ -0,0 +1,21 @@
+"""
+A module containing classes that represent the lattice.
+
+The lattice is a infinite periodic array of points in space. For the purpose of
+this program, only 3D lattices are used. Please see a typical reference such as
+"Structure and Dynamics" (Dove, 2001) or "The Structures of Crystals" (Glazer,
+1987) for more information.
+"""
+
+class Lattice:
+    """
+    A class representing a three-dimensional lattice.
+
+    Args:
+        length_a, length_b, length_c: floating point numbers representing the
+        magnitude (length) of the corresponding basic vectors in Angstroms.
+        angle_alpha, angle_beta, angle_gamma: floating-point numbers representing
+        the angle between the basis vectors b and c, a and c, and a and b,
+        respectively.
+    """
+    pass
