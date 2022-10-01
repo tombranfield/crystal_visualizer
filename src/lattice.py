6397eb5f2802d00a2310b1449d70077b16792503
Thomas Branfield <tbranfield@gmail.com>
2022-10-01 23:35:29 +0100

    Wrote property get/set for a in lattice

--- src/lattice.py
+++ src/lattice.py
@@ -12,22 +12,36 @@ class Lattice:
     A class representing a three-dimensional lattice.
 
     Args:
-        length_a, length_b, length_c: floating point numbers representing the
+        a, b, c: floating point numbers representing the
         magnitude (length) of the corresponding basic vectors in Angstroms.
-        angle_alpha, angle_beta, angle_gamma: floating-point numbers representing
+        alpha, beta, gamma: floating-point numbers representing
         the angle between the basis vectors b and c, a and c, and a and b,
         respectively.
     """
-    def __init__(self, length_a, length_b, length_c,
-                        angle_alpha, angle_beta, angle_gamma):
+    def __init__(self, a, b, c, alpha, beta, gamma):
         """Initialize the lattice using its lattice parameters"""
-        self.length_a = length_a
-        self.length_b = length_b
-        self.length_c = length_c
-        self.angle_alpha = angle_alpha
-        self.angle_beta = angle_beta
-        self.angle_gamma = angle_gamma
+        self.a = a
+        self.b = b
+        self.c = c
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
 
     @property
-    
+    def a(self):
+        """Returns the length of basis vector a"""
+        return self._a
 
+    @a.setter
+    def a(self, new_length: float):
+        """Verifies and sets a new length for the basis vector a"""
+        self.__verify_lattice_vector_length(new_length)
+        self._a = new_length
+
+    def __verify_lattice_vector_length(self, new_length):
+        """Verifies that a given input is a valid length for a lattice vector.
+
+        If the input is valid, no action is taken. Otherwise, an appropriate
+        exception is raised.
+        """
+        pass
