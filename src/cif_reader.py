051a076e283b8bffb661e52f007e9aac920ee176
Thomas Branfield <tbranfield@gmail.com>
2022-10-31 22:30:51 +0000

    Created cif_reader.py

---
+++ src/cif_reader.py
@@ -0,0 +1,9 @@
+"""
+This module contains the CifReader class that takes a cif file as input,
+and saves the information within that cif file into a CurrentCrystalData
+dataclass.
+"""
+
+
+class CifReader:
+    pass
