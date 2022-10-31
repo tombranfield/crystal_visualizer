f2a9acd6bfcc2be85981019e0fafa0a7ed793cd8
Thomas Branfield <tbranfield@gmail.com>
2022-10-31 23:42:38 +0000

    Renamed lattice parameters in lattice.py

--- src/cif_reader.py
+++ src/cif_reader.py
@@ -13,9 +13,27 @@ Hall, S.R., Allen, F.H, Brown, I.D. (1991). The Crystallographic Information
 File (CIF): A new standard archive file for Crystallography. Acta Crystallographica
 Section A Foundations of Crystallography, 47(6), 655-685.
 doi:10.1107/s010876739101067x
+
+Note not all the information from the CIF file is read.
+For example, experimental apparatus, authors, etc. This module concentrates
+on core structural information only of the lattice parameters, and
+atom identities and positions.
 """
 
-
-
 class CifReader:
-    pass
+    """
+    A module that reads CIF files
+    """
+
+    # _cell_length_a
+    # _cell_length_b
+    # _cell_length_c
+    # _cell_angle_alpha
+    # _cell_angle_beta
+    # _cell_angle_gamma
+
+    # _atom_site_fract_x
+    # _atom_site_fract_y
+
+    # _atom_type_oxidation_number
+    # _atom_type_radius_bond
--- src/lattice.py
+++ src/lattice.py
@@ -12,32 +12,28 @@ class Lattice:
     A class representing a three-dimensional lattice.
 
     Args:
-        a, b, c: floating point numbers representing the
+        length_a, length_b, length_c: floating point numbers representing the
         magnitude (length) of the corresponding basic vectors in Angstroms.
-        alpha, beta, gamma: floating-point numbers representing
+        angle_alpha, angle_beta, angle_gamma: floating-point numbers representing
         the angle between the basis vectors b and c, a and c, and a and b,
         respectively. In radians (NOT degrees).
     """
     def __init__(self, a, b, c, alpha, beta, gamma):
-        """Initialize the lattice using its lattice parameters.
-
-        We use single letters to represent the lengths of the basis vectors
-        as this is done by convention for crystallography.
-        """
-        self.a = a
-        self.b = b
-        self.c = c
-        self.alpha = alpha
-        self.beta = beta
-        self.gamma = gamma
+        """Initialize the lattice using its lattice parameters. """
+        self.length_a = a
+        self.length_b = b
+        self.length_c = c
+        self.angle_alpha = alpha
+        self.angle_beta = beta
+        self.angle_gamma = gamma
 
     @property
-    def a(self):
+    def length_a(self):
         """Returns the length of basis vector a"""
         return self._a
 
-    @a.setter
-    def a(self, new_length: float):
+    @length_a.setter
+    def length_a(self, new_length: float):
         """Verifies and sets a new length for the basis vector a"""
         self.__verify_lattice_vector_length(new_length)
         self._a = new_length
