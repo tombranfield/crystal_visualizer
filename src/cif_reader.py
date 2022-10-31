f06fbcd5332ac2012005c3917e29df5417fd060f
Thomas Branfield <tbranfield@gmail.com>
2022-10-31 22:47:27 +0000

    Updated explanation string of cif_reader.py

--- src/cif_reader.py
+++ src/cif_reader.py
@@ -1,8 +1,20 @@
 """
-This module contains the CifReader class that takes a cif file as input,
-and saves the information within that cif file into a CurrentCrystalData
-dataclass.
+The crystallographic information file (CIF) is the standard archival format
+for storing the information of crystal structures.
+
+This module contains the CifReader class that takes a CIF file as input,
+and saves the information within that CIF file into a CurrentCrystalData
+dataclass for use by the rest of the program.
+
+For more information about CIF files, please see:
+https://www.icur.org/resources/cif/spec/version1.1/cifsyntax
+or
+Hall, S.R., Allen, F.H, Brown, I.D. (1991). The Crystallographic Information
+File (CIF): A new standard archive file for Crystallography. Acta Crystallographica
+Section A Foundations of Crystallography, 47(6), 655-685.
+doi:10.1107/s010876739101067x
 """
+
 
 
 class CifReader:
