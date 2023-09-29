db7c314f2a803540f6e47665492820a0fd735770
Thomas Branfield <tbranfield@gmail.com>
2023-09-29 20:53:13 +0100

    Added two tests to test_metric_tensor for specific values

--- tests/test_metric_tensor.py
+++ tests/test_metric_tensor.py
@@ -53,3 +53,19 @@ def test_metric_tensor_for_rhombohedral_lattice():
         [cos_alpha, cos_alpha, 1]
     ])
     assert np.allclose(g, expected)
+
+
+# From example 4.3.1 pg.82 of "Structure of Materials"
+def test_metric_tensor_value_1():
+    lp = LatticeParameters(3, 4, 6, 90, 120, 90)
+    g = metric_tensor(lp)
+    expected = np.array([[9, 0, -9], [0, 16, 0], [-9, 0, 36]])
+    assert np.allclose(g, expected)
+
+
+# From example 4.3.2 pg.83 of "Structure of Materials"
+def test_metric_tensor_value_2():
+    lp = LatticeParameters(2, 2, 3, 90, 90, 90)
+    g = metric_tensor(lp)
+    expected = np.array([[4, 0, 0], [0, 4, 0], [0, 0, 9]])
+    assert np.allclose(g, expected)
