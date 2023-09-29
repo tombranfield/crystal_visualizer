eafabd0815f5244547f44e376c87ec54f73e514c
Thomas Branfield <tbranfield@gmail.com>
2023-09-29 20:47:52 +0100

    Wrote test_metric_tensor to compare with specific expressions for the 7 Bravais lattices

--- tests/test_metric_tensor.py
+++ tests/test_metric_tensor.py
@@ -7,8 +7,49 @@ from crystal_visualizer.lattice_parameters import LatticeParameters
 from crystal_visualizer.metric_tensor import metric_tensor
 
 
-def test_metric_tensor_for_cubic_crystal():
+def test_metric_tensor_for_cubic_lattice():
     lp = LatticeParameters(1, 1, 1, 90, 90, 90)
     g = metric_tensor(lp)
     expected = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-    assert np.allclose(g, expected) == True
+    assert np.allclose(g, expected)
+
+def test_metric_tensor_for_hexagonal_lattice():
+    lp = LatticeParameters(1, 1, 2, 90, 90, 120)
+    g = metric_tensor(lp)
+    expected = np.array([[1, -1/2, 0], [-1/2, 1, 0], [0, 0, 4]])
+    assert np.allclose(g, expected)
+
+def test_metric_tensor_for_monoclinic_lattice():
+    lp = LatticeParameters(1, 2, 3, 90, 135, 90)
+    g = metric_tensor(lp)
+    expected = ([
+        [1, 0, 3*np.cos(np.deg2rad(135))], 
+        [0, 4, 0], 
+        [3*np.cos(np.deg2rad(135)), 0, 9]]
+    )
+    assert np.allclose(g, expected)
+
+def test_metric_tensor_for_tetragonal_lattice():
+    lp = LatticeParameters(1, 1, 2, 90, 90, 90)
+    g = metric_tensor(lp)
+    expected = ([[1, 0, 0], [0, 1, 0], [0, 0, 4]])
+    assert np.allclose(g, expected)
+
+
+def test_metric_tensor_for_orthorhombic_lattice():
+    lp = LatticeParameters(1, 2, 3, 90, 90, 90)
+    g = metric_tensor(lp)
+    expected = ([[1, 0, 0], [0, 4, 0], [0, 0, 9]])
+    assert np.allclose(g, expected)
+
+
+def test_metric_tensor_for_rhombohedral_lattice():
+    lp = LatticeParameters(1, 1, 1, 110, 110, 110)
+    g = metric_tensor(lp)
+    cos_alpha = np.cos(np.deg2rad(110))
+    expected = ([
+        [1, cos_alpha, cos_alpha],
+        [cos_alpha, 1, cos_alpha],
+        [cos_alpha, cos_alpha, 1]
+    ])
+    assert np.allclose(g, expected)
