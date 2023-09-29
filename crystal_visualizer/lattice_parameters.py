6fe154170f0d27b21e333765bdeedf7e8ac59f5c
Thomas Branfield <tbranfield@gmail.com>
2023-09-29 15:45:51 +0100

    Wrote __verify_length() in lattice_parameters

--- crystal_visualizer/lattice_parameters.py
+++ crystal_visualizer/lattice_parameters.py
@@ -8,12 +8,17 @@ class LatticeParameters:
 
     Args:
         len_a, len_b, len_c are the lengths 
+        Angstroms
+        Angles in radians or degrees?
+
     """
     def __init__(
         self, 
         len_a, len_b, len_c, 
         angle_alpha, angle_beta, angle_gamma
     ):
+        self.__verify_length(len_a, len_b, len_c)
+
         self._len_a = len_a
         self._len_b = len_b
         self._len_c = len_c
@@ -25,14 +30,13 @@ class LatticeParameters:
     def length_a(self):
         return self._len_a
 
-
-
-    def __verify_length(self, length):
-        if not isinstance(length, (float, int)):
-            raise TypeError("Length must be a number")
-        if length <= 0:
-            raise ValueError("Length must be a positive number")
+    def __verify_length(self, *lengths):
+        for length in lengths:
+            if not isinstance(length, (float, int)):
+                raise TypeError("Length must be a number")
+            if length <= 0:
+                raise ValueError("Length must be a positive number")
 
 
 if __name__ == "__main__":
-    print("yo")
+    my_lattice = LatticeParameters(1, 1, 1, 90, 90, 90)
