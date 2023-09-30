f93771cd4b5accb1bf9fb655cfeb4e2f04349dfa
Thomas Branfield <tbranfield@gmail.com>
2023-09-30 23:13:45 +0100

    Wrote test for reciprocal metric tensor with a monoclinic lattice

--- tests/test_reciprocal_metric_tensor.py
+++ tests/test_reciprocal_metric_tensor.py
@@ -28,6 +28,7 @@ def test_volume_of_triclinic_lattice_correctly_direct_space():
     expected_volume = np.sqrt(3888)
     assert np.allclose(calculated_volume, expected_volume)
 
+
 # From pg.114 "Structure of Materials"
 def test_reciprocal_metric_tensor_for_cubic_lattice():
     lp = LatticeParameters(2, 2, 2, 90, 90, 90)
@@ -41,6 +42,12 @@ def test_reciprocal_metric_tensor_for_triclinic_lattice():
     lp = LatticeParameters(3, 4, 6, 90, 90, 120)
     calculated = reciprocal_metric_tensor(lp)
     expected = np.array([[4/27, 1/18, 0], [1/18, 1/12, 0], [0, 0, 1/36]])
-    print(calculated)
-    print(expected)
+    assert np.allclose(calculated, expected)
+
+
+# From example (iii) pg.117 "Structure of Materials"
+def test_reciprocal_metric_tensor_for_monoclinic_lattice():
+    lp = LatticeParameters(4, 6, 5, 90, 120, 90)
+    calculated = reciprocal_metric_tensor(lp)
+    expected = np.array([[1/12, 0, 1/30], [0, 1/36, 0], [1/30, 0, 4/75]])
     assert np.allclose(calculated, expected)
