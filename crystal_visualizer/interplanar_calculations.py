32e3ed039b7b5452959e78dfdfc4f508ccc6e4e1
Thomas Branfield <tbranfield@gmail.com>
2023-09-30 23:46:15 +0100

    Added test_interplanar_calculations.py

--- crystal_visualizer/interplanar_calculations.py
+++ crystal_visualizer/interplanar_calculations.py
@@ -2,17 +2,19 @@
 
 import numpy as np
 
+from crystal_visualizer.hkl import HKL
 from crystal_visualizer.lattice_parameters import LatticeParameters
 from crystal_visualizer.reciprocal_metric_tensor import reciprocal_metric_tensor
 
 
-def interplanar_spacing(
+def interplanar_distance(
     hkl: HKL, 
     lattice_parameters: LatticeParameters
 ) -> float:
     indices = np.array([hkl.h, hkl.k, hkl.l])
     g = reciprocal_metric_tensor(lattice_parameters)    
     g_length = np.sqrt(indices @ (g @ indices))
+    print(g_length)
     return 1/g_length
 
 
---
+++ tests/test_interplanar_calculations.py
@@ -0,0 +1,16 @@
+"""test_interplanar_calculations.py"""
+
+import numpy as np
+
+from crystal_visualizer.hkl import HKL
+from crystal_visualizer.interplanar_calculations import interplanar_distance
+from crystal_visualizer.lattice_parameters import LatticeParameters
+
+
+# From example (i) on pg.114 of "Structure of Materials"
+def test_interplanar_spacing_successfully_calculated_for_cubic():
+    lp = LatticeParameters(2, 2, 2, 90, 90, 90)
+    plane_hkl = HKL(1, 1, 0)
+    calculated_spacing = interplanar_distance(plane_hkl, lp)
+    expected_spacing = 1.414
+    assert np.allclose(calculated_spacing, expected_spacing)
