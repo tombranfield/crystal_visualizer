5c347676a9953b7a1aa583f50c5722a7d188e4a1
Thomas Branfield <tbranfield@gmail.com>
2023-09-30 21:39:49 +0100

    Wrote 3 tests for volume() in test_reciprocal_metric_tensor.py

--- tests/test_reciprocal_metric_tensor.py
+++ tests/test_reciprocal_metric_tensor.py
@@ -11,5 +11,19 @@ from crystal_visualizer.reciprocal_metric_tensor import volume
 def test_volume_of_cube_calculated_correctly_direct_space():
     lp = LatticeParameters(10, 10, 10, 90, 90, 90)
     calculated_volume = volume(lp)
-    predicted_volume = 1000
-    assert calculated_volume == predicted_volume
+    expected_volume = 1000
+    assert calculated_volume == expected_volume
+
+
+def test_volume_of_tetrahedron_calculated_correctly_direct_space():
+    lp = LatticeParameters(2, 3, 4, 90, 90, 90)
+    calculated_volume = volume(lp)
+    expected_volume = 24
+    assert expected_volume == expected_volume
+
+
+def test_volume_of_triclinic_lattice_correctly_direct_space():
+    lp = LatticeParameters(3, 4, 6, 90, 90, 120)
+    calculated_volume = volume(lp)
+    expected_volume = np.sqrt(3888)
+    assert np.allclose(calculated_volume, expected_volume)
