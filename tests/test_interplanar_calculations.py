10c5b7ac429bfd8422064c27837f51efde37a43f
Thomas Branfield <tbranfield@gmail.com>
2023-10-01 11:10:52 +0100

    Added two more tests for interplanar spacing in interplanar_calculations.py

--- tests/test_interplanar_calculations.py
+++ tests/test_interplanar_calculations.py
@@ -17,7 +17,18 @@ def test_interplanar_spacing_successfully_calculated_for_cubic():
     assert calculated_spacing_3dp == expected_spacing
 
 
-def test_interplanar_angle():
+# From example (ii) pg.115 of "Structure of Materials"
+def test_interplanar_spacing_successfully_calculated_for_monoclinic():
+    lp = LatticeParameters(3, 4, 6, 90, 90, 120)
+    plane_hkl = HKL(1, 1, 1)
+    calculated_spacing = interplanar_distance(plane_hkl, lp)
+    calculated_spacing_3dp = float(str(calculated_spacing)[:5])
+    expected_spacing = 1.643
+    assert calculated_spacing_3dp == expected_spacing
+
+
+# From example (iii) pg.117 of "Structure of Materials"
+def test_interplanar_angle_calculated_correctly():
     lp = LatticeParameters(4, 6, 5, 90, 120, 90)
     hkl_1 = HKL(1, 0, 1)
     hkl_2 = HKL(-2, 0, 1)
@@ -25,3 +36,21 @@ def test_interplanar_angle():
     calculated_angle_3dp = float(str(calculated_angle)[:6])
     expected_angle = 130.25
     assert calculated_angle_3dp == expected_angle
+
+
+# From selected problems pg.120 of "Structure of Materials"
+def test_interplanar_distances_of_goethite_calculated_correctly():
+    lp = LatticeParameters(0.4596, 0.9957, 0.3021, 90, 90, 90)
+    plane_1 = HKL(0,2,0)
+    plane_2 = HKL(2,0,0)
+    plane_3 = HKL(1,1,1)
+    calculated_spacing_1 = round(interplanar_distance(plane_1, lp), 3)
+    calculated_spacing_2 = round(interplanar_distance(plane_2, lp), 3)
+    calculated_spacing_3 = round(interplanar_distance(plane_3, lp), 3)
+    expected_1 = 0.498
+    expected_2 = 0.230
+    expected_3 = 0.245
+    assert calculated_spacing_1 == expected_1
+    assert calculated_spacing_2 == expected_2
+    assert calculated_spacing_3 == expected_3
+    
