68ad6b45f79c521b143ce24c7b433ae9785041b6
Thomas Branfield <tbranfield@gmail.com>
2023-10-03 21:26:38 +0100

    Added unit_cell.py

---
+++ crystal_visualizer/unit_cell.py
@@ -0,0 +1,15 @@
+"""unit_cell.py"""
+
+# Consists of (1) list of atoms (2) lattice parameters
+# Has a label or chemical formula
+
+
+from crystal_visualizer.atom import Atom
+from crystal_visualizer.lattice_parameters import Lattice Parameters
+
+
+class UnitCell:
+    """
+    Unit cell of the crystal.
+    """
+    pass
