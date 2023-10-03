3d58ec8ca71cc4e8289bd70a3783e67053133a48
Thomas Branfield <tbranfield@gmail.com>
2023-10-03 21:38:18 +0100

    Wrote init() and symbol() methods for space_group

--- crystal_visualizer/space_group.py
+++ crystal_visualizer/space_group.py
@@ -1,3 +1,23 @@
 """space_group.py"""
 
 
+class SpaceGroup:
+    """
+    A space group representing symmetry of a repeating 3D pattern.
+
+    In:
+        The space group symbol in Hermann_Mauguin notation.
+
+    """
+    def __init__(self, symbol):
+        """Initializes the space group using its symbol"""
+        self._symbol = symbol
+
+    @property
+    def symbol(self):
+        return self.symbol
+
+    def __get_data(self):
+        # Gets the data for the space group from the Internatinal
+        # Tables of Crystallography
+        pass
--- crystal_visualizer/unit_cell.py
+++ crystal_visualizer/unit_cell.py
@@ -1,11 +1,12 @@
 """unit_cell.py"""
 
 # Consists of (1) list of atoms (2) lattice parameters
-# Has a label or chemical formula
+# Has a label or chemical formula (3) Space Group
 
 
 from crystal_visualizer.atom import Atom
-from crystal_visualizer.lattice_parameters import Lattice Parameters
+from crystal_visualizer.lattice_parameters import LatticeParameters
+from crystal_visualizer.space_group import SpaceGroup
 
 
 class UnitCell:
