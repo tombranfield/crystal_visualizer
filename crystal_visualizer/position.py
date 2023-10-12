a933ca1fb8260058774dd9ffc03d8be64b6b94e8
Thomas Branfield <tbranfield@gmail.com>
2023-10-12 13:01:53 +0100

    Added position.py

---
+++ crystal_visualizer/position.py
@@ -0,0 +1,20 @@
+"""position.py"""
+
+
+class Position:
+    """
+    A class representing position.
+    
+    Args:
+        fract_x, fract_y, fract_z: floating point numbers representing the 
+        fractional coordinates of the atom within the unit cell.
+    """
+    def __init__(self, fract_x: float, fract_y: float, fract_z: float):
+        """Initializes the Position class"""
+        self.fract_x = fract_x
+        self.fract_y = fract_y
+        self.fract_z = fract_z
+
+    @property
+    def coods(self) -> np.array:
+        """Returns the position vector of the array"""
