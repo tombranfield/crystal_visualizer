20cd34a93dd1f026bd5147f108033898e3e1fc1a
Thomas Branfield <tbranfield@gmail.com>
2023-10-12 21:33:48 +0100

    Wrote test for retrieving coods in test_position.py

--- tests/test_position.py
+++ tests/test_position.py
@@ -1,5 +1,6 @@
 """test_position.py"""
 
+import numpy as np
 
 from crystal_visualizer.position import Position
 
@@ -7,3 +8,17 @@ from crystal_visualizer.position import Position
 
 def test_can_instantiate_position_successfully():
     position = Position(0.5, 0.5, 0.5)
+
+
+def test_can_retrieve_coods_array_successfully():
+    position = Position(0.5, 0.25, 0.5)
+    generated_coods = position.coods()
+    expected_coods = np.array([0.5, 0.25, 0.5])
+    assert np.allclose(generated_coods, expected_coods)
+
+"""
+def test_can_change_position_successfully():
+    position = Position(0.25, 0.5, 0.5)
+    position.x += 0.5
+    assert position.coods == np.array([0.75, 0.5, 0.5])
+"""
