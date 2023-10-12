e502b53c0783c1ed070980e3c2c3d80f7228a876
Thomas Branfield <tbranfield@gmail.com>
2023-10-12 22:48:01 +0100

    Renamed cif_file_parser to cif_reader

--- crystal_visualizer/cif_reader.py
+++ crystal_visualizer/cif_reader.py
@@ -2,10 +2,6 @@
 The crystallographic information file (CIF) is the standard archival format
 for storing the information of crystal structures.
 
-This module contains the CifReader class that takes a CIF file as input,
-and saves the information within that CIF file into a CurrentCrystalData
-dataclass for use by the rest of the program.
-
 For more information about CIF files, please see:
 https://www.icur.org/resources/cif/spec/version1.1/cifsyntax
 or
@@ -13,17 +9,22 @@ Hall, S.R., Allen, F.H, Brown, I.D. (1991). The Crystallographic Information
 File (CIF): A new standard archive file for Crystallography. Acta Crystallographica
 Section A Foundations of Crystallography, 47(6), 655-685.
 doi:10.1107/s010876739101067x
-
-Note not all the information from the CIF file is read.
-For example, experimental apparatus, authors, etc. This module concentrates
-on core structural information only of the lattice parameters, and
-atom identities and positions.
 """
+
+from pathlib import Path
+
 
 class CifReader:
     """
     A module that reads CIF files
     """
+    PATH = str(Path(__file__) / "data" / "cif_files")
+
+    def __init__(self, filename: str)
+        """Initialize the Cif reader"""
+        self.file_path = PATH + filename
+        print(self.file_path)
+
 
     # _cell_length_a
     # _cell_length_b
