85d91b91115c8db21db2da972501bc25384c00ea
Thomas Branfield <tbranfield@gmail.com>
2023-10-12 18:58:38 +0100

    Added test_atom.py

---
+++ tests/test_atom.py
@@ -0,0 +1,15 @@
+"""test_atom.py"""
+
+import pytest
+
+from crystal_visualizer.atom import Atom
+
+
+
+def test_initialize_atom_successfully():
+    atom = Atom("Ba", 0.5, 0.5, 0.5)
+
+
+
+def test_can_change_label_successfully():
+    pass
