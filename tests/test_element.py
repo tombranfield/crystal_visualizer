75d24121b7c81d1992a6f008a9f7a70bed9e42eb
Thomas Branfield <tbranfield@gmail.com>
2023-10-12 18:55:49 +0100

    Wrote two more tests for test_element.py

--- tests/test_element.py
+++ tests/test_element.py
@@ -1,5 +1,7 @@
 """test_element.py"""
 
+import pytest
+
 from crystal_visualizer.element import Element
 
 
@@ -10,3 +12,14 @@ def test_initialize_barium_retrieve_properties_successfully():
     assert element.name == "Barium"
     assert element.atomic_number == 56
     assert element.atomic_radius == 2.53
+
+
+def test_initialize_lowercase_element_initializes_successfully():
+    element = Element("ba")
+    assert element.symbol == "Ba"
+
+
+def test_initializing_non_existant_element_gives_error():
+    with pytest.raises(ValueError):
+        element = Element("XY")
+        
