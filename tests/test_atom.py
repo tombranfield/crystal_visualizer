a242af049e13bc9e5917f31dc99f4fafe3479356
Thomas Branfield <tbranfield@gmail.com>
2023-10-12 19:07:02 +0100

    Added element properties test to test_atom

--- tests/test_atom.py
+++ tests/test_atom.py
@@ -5,10 +5,16 @@ import pytest
 from crystal_visualizer.atom import Atom
 
 
-
 def test_initialize_atom_successfully():
     atom = Atom("Ba", 0.5, 0.5, 0.5)
 
+
+def test_retrieve_correct_element_properties_from_atom(barium):
+    atom = Atom("Ba", 0.5, 0.5, 0.5)
+    assert atom.element.symbol == "Ba"
+    assert atom.element.atomic_number == 56
+    assert atom.element.atomic_radius == 2.53
+    assert atom.element.name == "Barium"
 
 
 def test_can_change_label_successfully():
