d0a02b681e26ce091595c0ee2bf86d07bd478861
Thomas Branfield <tbranfield@gmail.com>
2023-10-13 12:22:29 +0100

    Added tests for CsCl and diamond lattice parameters

--- tests/test_cif_reader.py
+++ tests/test_cif_reader.py
@@ -40,5 +40,25 @@ def test_generate_correct_lattice_parameters_for_CaF2():
     assert cif_reader.lattice_parameters.angle_gamma == 90
 
 
+def test_generate_correct_lattice_parameters_for_diamond():
+    cif_reader = CifReader("C.cif")
+    assert cif_reader.lattice_parameters.length_a == 3.54
+    assert cif_reader.lattice_parameters.length_b == 3.54
+    assert cif_reader.lattice_parameters.length_c == 3.54
+    assert cif_reader.lattice_parameters.angle_alpha == 90
+    assert cif_reader.lattice_parameters.angle_beta == 90
+    assert cif_reader.lattice_parameters.angle_gamma == 90
+
+
+def test_generate_correct_lattice_parameters_for_CsCl():
+    cif_reader = CifReader("CsCl.cif")
+    assert cif_reader.lattice_parameters.length_a == 4.123
+    assert cif_reader.lattice_parameters.length_b == 4.123
+    assert cif_reader.lattice_parameters.length_c == 4.123
+    assert cif_reader.lattice_parameters.angle_alpha == 90
+    assert cif_reader.lattice_parameters.angle_beta == 90
+    assert cif_reader.lattice_parameters.angle_gamma == 90
+
+
 if __name__ == "__main__":
     print(CIF_PATH)
