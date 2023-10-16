448cc3860096543e2e734565fe20bb3116ce53bb
Thomas Branfield <tbranfield@gmail.com>
2023-10-16 20:59:53 +0100

    Added sym_op_str_to_pos() in general_pos_generator.py

--- crystal_visualizer/general_pos_generator.py
+++ crystal_visualizer/general_pos_generator.py
@@ -2,6 +2,7 @@
 
 
 from crystal_visualizer.cif_reader import CifReader
+from crystal_visualizer.position import Position
 
 
 class GeneralPositions:
@@ -13,26 +14,25 @@ class GeneralPositions:
         self.starting_atoms = starting_atoms
         self.symmetry_ops = symmetry_ops
 
-    def generate_general_positions(self):
-        # Go through each atom
-        # For each atom, generate Position vectors using the sym ops
-        # Add each new Position vector to a list
-        # Eliminate any duplicate Positions
+    def generate(self) -> Atom:
         for atom in self.starting_atoms:
-            cur_pos = atom.position_vector()
-            print(cur_pos)
+            atom_pos = atom.position_vector()
+            x, y, z = atom_pos[0], atom_pos[1], atom_pos[2]
+            for sym_op in self.symmetry_ops[:1]:
+                x_sym_op = sym_op[0]
+                y_sym_op = sym_op[1]
+                z_sym_op = sym_op[2]
+                
+                # Now take the sym op string, convert to a position vec
+                # So we need to generate new float x,y,z to make new Position
+                if "+" in x_sym_op:
+                    x_sym_op.split("+")
 
-
+    def _sym_op_str_to_pos(self, sym_op_str):
+        pass
 
 if __name__ == "__main__":
     cif_reader = CifReader("Cu.cif")
-    """
-    for atom in cif_reader.atoms:
-        print(atom.symbol)
-        print(atom.position_vector())
-    print(cif_reader.symmetry_ops.sym_ops)
-    """    
-    
     atoms = cif_reader.atoms
     sym_ops = cif_reader.symmetry_ops.sym_ops
     general_positions = GeneralPositions(atoms, sym_ops)
