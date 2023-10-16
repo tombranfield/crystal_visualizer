8e922fd00937d91601c260c4498686ea79e34ee1
Thomas Branfield <tbranfield@gmail.com>
2023-10-16 12:49:16 +0100

    Turned SpaceGroupSymOps into a class, added sym_ops property

--- crystal_visualizer/space_group_symmetry_ops.py
+++ crystal_visualizer/space_group_symmetry_ops.py
@@ -1,9 +1,22 @@
 """space_group_symmetry_ops"""
 
 
-from dataclasses import dataclass
-
-
-@dataclass
 class SpaceGroupSymOps:
-    sym_ops: list[str]
+    """
+    Represents the symmetry operations for generating general equivalent
+    positions of a space group. 
+    """
+    def __init__(self, sym_ops=None):
+        self._sym_ops = sym_ops
+
+    @property
+    def sym_ops(self):
+        return self._sym_ops
+
+
+
+
+if __name__ == "__main__":
+    ops = [["x", "y", "z"], ["-x", "y", "z"]]
+    my_sym_ops = SpaceGroupSymOps(ops)
+    print(my_sym_ops.sym_ops)
