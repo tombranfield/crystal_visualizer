af7a9930d2922b1e33da37420f2158ac653bd6ac
Thomas Branfield <tbranfield@gmail.com>
2023-10-17 00:06:46 +0100

    Fixed issue in sym_op_str_to_pos where all branches were accessed due to faulty logic

--- crystal_visualizer/general_pos_generator.py
+++ crystal_visualizer/general_pos_generator.py
@@ -4,35 +4,48 @@
 from crystal_visualizer.atom import Atom
 from crystal_visualizer.cif_reader import CifReader
 from crystal_visualizer.position import Position
+from crystal_visualizer.space_group_symmetry_ops import SpaceGroupSymOps
 
 
 class GeneralPositionGenerator:
     """
     Generate a set of general positions given some starting atoms
     and a set of symmetry operations.
+
+    In:
+        atom: a 
+
     """
-    def __init__(self, starting_atoms, symmetry_ops):
-        self.starting_atoms = starting_atoms
+    def __init__(self, atom: Atom, symmetry_ops: SpaceGroupSymOps):
+        self.atom = atom
         self.symmetry_ops = symmetry_ops
 
-
     def generate(self) -> Atom:
-        for atom in self.starting_atoms:
-            orig_atom_pos = atom.position_vector()
-            atom_positions = []
-            for sym_op in self.symmetry_ops:
-                x_op, y_op, z_op = sym_op[0], sym_op[1], sym_op[2]
-                new_x = self._sym_op_str_to_pos(orig_atom_pos, x_op)
-                new_y = self._sym_op_str_to_pos(orig_atom_pos, y_op)
-                new_z = self._sym_op_str_to_pos(orig_atom_pos, z_op)
-                new_position = Position(new_x, new_y, new_z)
-                if new_position not in atom_positions:
-                    atom_positions.append(new_position)
-            self.print_new_pos(atom_positions)
+        """
+        Output
+        """
+        orig_atom_pos = self.atom.position_vector()
+        print("orig pos:", orig_atom_pos)
+        atom_positions = []
+        for sym_op in self.symmetry_ops[:1]:
+            x_op, y_op, z_op = sym_op[0], sym_op[1], sym_op[2]
+            new_x = self._sym_op_str_to_pos(orig_atom_pos, x_op)
+            new_y = self._sym_op_str_to_pos(orig_atom_pos, y_op)
+            new_z = self._sym_op_str_to_pos(orig_atom_pos, z_op)
+            
+            new_position = Position(new_x, new_y, new_z)
+            # TODO eliminate positions outside unit cell
+#            if (self._is_pos_in_unit_cell(new_position)
+            print(sym_op, end=" ")
+            print(new_position.coods())
 
+            if new_position not in atom_positions:
+                atom_positions.append(new_position)
+#        self.print_new_pos(atom_positions)
 
-    def _sym_op_str_to_pos(self, atom_pos, sym_op_str) -> float:
-        x, y, z = atom_pos[0], atom_pos[1], atom_pos[2]
+    # TODO this doesn't work - need to parse correctly
+    def _sym_op_str_to_pos(self, orig_atom_pos, sym_op_str) -> float:
+        x, y, z = orig_atom_pos[0], orig_atom_pos[1], orig_atom_pos[2]
         output = 0.0
         if "+" in sym_op_str:
             op_parts = sym_op_str.split("+")
@@ -41,11 +54,11 @@ class GeneralPositionGenerator:
                     output += self._fraction_str_to_float(op_part)
                 else:
                     sym_op_str = op_part
-        if "x" and not "-x" in sym_op_str:
+        if "x" in sym_op_str:
             output += x
-        if "y" and not "-y" in sym_op_str:
+        if "y" in sym_op_str:
             output += y
-        if "z" and not "-z" in sym_op_str:
+        if "z" in sym_op_str:
             output += z
         if "-x" in sym_op_str:
             output -= x
@@ -63,10 +76,18 @@ class GeneralPositionGenerator:
             nom, denom = fraction_str.split("/")
             return float(nom) / float(denom)
 
+    def _is_pos_in_unit_cell(self, position):
+        if (position.x >= 1.0 or
+            position.y >= 1.0 or
+            position.z >= 1.0):
+            return False
+        return True
+
     
     def print_new_pos(self, new_pos_array):
         for new_pos in new_pos_array:
             print(new_pos.coods())
+        print(len(new_pos_array))
 
 
     def remove_duplicate_pos(self, new_pos_array):
@@ -74,9 +95,26 @@ class GeneralPositionGenerator:
 
 
 if __name__ == "__main__":
+    """
+    # Copper test
     cif_reader = CifReader("Cu.cif")
-    atoms = cif_reader.atoms
+    atom = cif_reader.atoms[0]
     sym_ops = cif_reader.symmetry_ops.sym_ops
-    print(sym_ops)
-    general_positions = GeneralPositionGenerator(atoms, sym_ops)
+    general_positions = GeneralPositionGenerator(atom, sym_ops)
     general_positions.generate()
+    """
+    
+
+
+    # NaCl test
+    cif_reader = CifReader("NaCl.cif")
+    na_atom = cif_reader.atoms[0]
+    cl_atom = cif_reader.atoms[1]
+    sym_ops = cif_reader.symmetry_ops.sym_ops
+
+#    print("Na:")
+    na_general_positions = GeneralPositionGenerator(na_atom, sym_ops)
+#    na_general_positions.generate()
+    print("Cl:")
+    cl_general_positions = GeneralPositionGenerator(cl_atom, sym_ops)
+    cl_general_positions.generate()
