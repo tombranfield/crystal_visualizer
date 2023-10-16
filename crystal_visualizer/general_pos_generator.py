12178d314c94fe7b89b2dce1ccdaf1837cf31869
Thomas Branfield <tbranfield@gmail.com>
2023-10-16 21:34:05 +0100

    sym_op_str_to_pos now accounts for negative signs and constants

--- crystal_visualizer/general_pos_generator.py
+++ crystal_visualizer/general_pos_generator.py
@@ -15,11 +15,12 @@ class GeneralPositions:
         self.starting_atoms = starting_atoms
         self.symmetry_ops = symmetry_ops
 
+
     def generate(self) -> Atom:
         for atom in self.starting_atoms:
             atom_pos = atom.position_vector()
             new_positions = []
-            for sym_op in self.symmetry_ops[:2]:
+            for sym_op in self.symmetry_ops:
                 x_op, y_op, z_op = sym_op[0], sym_op[1], sym_op[2]
                 new_x = self._sym_op_str_to_pos(atom_pos, x_op)
                 new_y = self._sym_op_str_to_pos(atom_pos, y_op)
@@ -33,23 +34,32 @@ class GeneralPositions:
     def _sym_op_str_to_pos(self, atom_pos, sym_op_str) -> float:
         x, y, z = atom_pos[0], atom_pos[1], atom_pos[2]
         output = 0.0
-        # First account for possible constants
         if "+" in sym_op_str:
-            pass
-            # find the bit with letters in
-            # find the part with numbers in, read, and add
-        if "x" in sym_op_str:
+            op_parts = sym_op_str.split("+")
+            for op_part in op_parts:
+                if "/" in op_part:
+                    output += float(op_part)
+                else:
+                    sym_op_str = op_part
+        if "x" and not "-x" in sym_op_str:
             output += x
-        if "y" in sym_op_str:
+        if "y" and not "-y" in sym_op_str:
             output += y
-        if "z" in sym_op_str:
+        if "z" and not "-z" in sym_op_str:
             output += z
+        if "-x" in sym_op_str:
+            output -= x
+        if "-y" in sym_op_str:
+            output -= y
+        if "-z" in sym_op_str:
+            output -= z
         return output
 
     
     def print_new_pos(self, new_pos_array):
         for new_pos in new_pos_array:
             print(new_pos.coods())
+
 
     def remove_duplicate_pos(self, new_pos_array):
         pass
@@ -59,5 +69,6 @@ if __name__ == "__main__":
     cif_reader = CifReader("Cu.cif")
     atoms = cif_reader.atoms
     sym_ops = cif_reader.symmetry_ops.sym_ops
+    print(sym_ops)
     general_positions = GeneralPositions(atoms, sym_ops)
     general_positions.generate()
