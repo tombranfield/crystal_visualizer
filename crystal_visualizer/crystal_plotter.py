799a6d2e7256835c312e9eb962ddaabd80913a0a
Thomas Branfield <tbranfield@gmail.com>
2023-10-20 20:32:09 +0100

    Wrote plot() for CrystalPlotter

--- crystal_visualizer/crystal_plotter.py
+++ crystal_visualizer/crystal_plotter.py
@@ -9,34 +9,35 @@ from crystal_visualizer.cif_to_unit_cell import cif_to_unit_cell
 from crystal_visualizer.unit_cell import UnitCell
 
 
-
-
 class CrystalPlotter:
     """
     Plots a crystal structure given its unit cell
     """
     def __init__(self, unit_cell: UnitCell):
         self.unit_cell = unit_cell
-        self.fig, self.ax = plt.subplots(subplot_kw={"projection": "3d"})
 
     def plot(self):
-        list_centre = []
-        list_radius = []
-        list_colour = []
+        fig = plt.figure()
+        ax = fig.add_subplot(projection="3d")
         for atom in self.unit_cell.atoms:
-            list_centre.append(atom.position_vector())
-            list_radius.append(atom.element.atomic_radius)
-            list_colour.append(atom.element.colour)
+            radius = 0.5 * atom.element.atomic_radius
+            a = self.unit_cell.lattice_param.length_a
+            b = self.unit_cell.lattice_param.length_a
+            c = self.unit_cell.lattice_param.length_a
+            u, v = np.mgrid[0:2*np.pi:50j, 0:np.pi:50j]
+            x = radius * np.cos(u) * np.sin(v)
+            y = radius * np.sin(u) * np.sin(v)
+            z = radius * np.cos(v)
+            ax.plot_surface(x - a * atom.position.x,
+                            y - b * atom.position.y,
+                            z - c * atom.position.z,
+                            color=atom.element.colour)
+        plt.show()
 
-        print(list_centre)
-        print(list_radius)
-        print(list_colour)
 
 
 
 if __name__ == "__main__":
-    unit_cell = cif_to_unit_cell("Cu.cif")
-    unit_cell.print_info()
-
+    unit_cell = cif_to_unit_cell("SrTiO3.cif")
     crystal_plotter = CrystalPlotter(unit_cell)
     crystal_plotter.plot()
