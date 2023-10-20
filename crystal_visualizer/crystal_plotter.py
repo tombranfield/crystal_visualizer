d173d78f9dfa94f20adbad1bf2eccd2550132d8d
Thomas Branfield <tbranfield@gmail.com>
2023-10-20 22:11:18 +0100

    Fixed an issue in crystal plotter where positions were negative of what they should have been

--- crystal_visualizer/crystal_plotter.py
+++ crystal_visualizer/crystal_plotter.py
@@ -1,5 +1,6 @@
 #TODO draw the unit cell edges
 #TODO handle non-orthogonal coordinates
+#TODO fix the positions - they're plotted as negative
 
 
 """crystal_plotter.py"""
@@ -23,6 +24,9 @@ class CrystalPlotter:
     def plot(self):
         fig = plt.figure()
         ax = fig.add_subplot(aspect="equal", projection="3d")
+
+        # TODO refactor this, obviously
+        # Plot the spheres
         for atom in self.unit_cell.atoms:
             radius = 0.5 * atom.element.atomic_radius
             a = self.unit_cell.lattice_param.length_a
@@ -36,12 +40,25 @@ class CrystalPlotter:
             x = radius * np.cos(u) * np.sin(v)
             y = radius * np.sin(u) * np.sin(v)
             z = radius * np.cos(v)
-            ax.plot_surface(x - a * atom.position.x,
-                            y - b * atom.position.y,
-                            z - c * atom.position.z,
+            ax.plot_surface(a * atom.position.x - x,
+                            b * atom.position.y - x,
+                            c * atom.position.z - z,
                             color=atom.element.colour)
+        
+        # Plot cell edges (lines)
+        # Start with a single line in orthogonal space
+        x = np.array([0, a])
+        y = np.array([0, 0])
+        z = np.array([0, 0])
+        ax.plot3D(x, y, z)
+
+        # Plot appearance
         z_stretch_ratio = c / a
         ax.set_box_aspect((1, 1, z_stretch_ratio))
+        ax.set_xlabel("a / r’$\AA$’")
+        ax.set_ylabel("b / r’$\AA$’")
+        ax.set_zlabel("c / r’$\AA$’")
+
         plt.show()
 
 
