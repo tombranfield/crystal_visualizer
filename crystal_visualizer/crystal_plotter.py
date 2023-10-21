01426f4b7da0ea5997164c5e9d5fd2bce8a5761b
Thomas Branfield <tbranfield@gmail.com>
2023-10-21 15:20:20 +0100

    Removed unncessary quotation marks and 'r' from  axes labels in crystal_plotter

--- crystal_visualizer/crystal_plotter.py
+++ crystal_visualizer/crystal_plotter.py
@@ -25,28 +25,9 @@ class CrystalPlotter:
         fig = plt.figure(figsize=(8, 6))
         ax = fig.add_subplot(aspect="equal", projection="3d")
 
-        """
-        # Plot the edges of the unit cell
-        # The below is old and for orthogonal coods only
-        lines = [
-            [(0, a), (0, 0), (0, 0)],  [(0, 0), (0, b), (0, 0)],
-            [(a, a), (0, b), (0, 0)],  [(0, a), (b, b), (0, 0)],
-            [(0, 0), (0, 0), (0, c)],  [(0, 0), (b, b), (0, c)],
-            [(a, a), (0, 0), (0, c)],  [(a, a), (b, b), (0, c)],
-            [(0, a), (0, 0), (c, c)],  [(0, 0), (0, b), (c, c)],
-            [(a, a), (0, b), (c, c)],  [(0, a), (b, b), (c, c)],
-        ]
-        for line in lines:
-            x, y, z = line[0], line[1], line[2]
-            ax.plot3D(x, y, z, color="red")
-        """
         a = list(self.basis_vectors.a)
         b = list(self.basis_vectors.b)
         c = list(self.basis_vectors.c)
-
-        print("basis vector a", a)
-        print("basis vector b", b)
-        print("basis vector c", c)
 
         lines = [
             [(0, 1), (0, 0), (0, 0)],  [(0, 0), (0, 1), (0, 0)],
@@ -65,20 +46,13 @@ class CrystalPlotter:
             x = (r_before[0], r_after[0])
             y = (r_before[1], r_after[1])
             z = (r_before[2], r_after[2])
-            """
-            print("\n------------")
-            print("x", x)
-            print("y", y)
-            print("z", z)
-            print("------------\n")
-            """
             ax.plot3D(x, y, z, color="red")
 
 
 
         # Plot the spheres
         for atom in self.unit_cell.atoms:
-            radius_scale = 0.35
+            radius_scale = 0.4
             radius = radius_scale * atom.element.atomic_radius
             len_a = self.unit_cell.lattice_param.length_a
             len_b = self.unit_cell.lattice_param.length_b
@@ -100,9 +74,9 @@ class CrystalPlotter:
         # Plot appearance
         z_stretch_ratio = len_c / len_a
         ax.set_box_aspect((1, 1, z_stretch_ratio))
-        ax.set_xlabel("a / r’$\AA$’")
-        ax.set_ylabel("b / r’$\AA$’")
-        ax.set_zlabel("c / r’$\AA$’")
+        ax.set_xlabel("a / $\AA$")
+        ax.set_ylabel("b / $\AA$")
+        ax.set_zlabel("c / $\AA$")
         fig.tight_layout()
         plt.show()
 
@@ -110,6 +84,6 @@ class CrystalPlotter:
 
 
 if __name__ == "__main__":
-    unit_cell = cif_to_unit_cell("SiO2.cif")
+    unit_cell = cif_to_unit_cell("SrTiO3.cif")
     crystal_plotter = CrystalPlotter(unit_cell)
     crystal_plotter.plot()
