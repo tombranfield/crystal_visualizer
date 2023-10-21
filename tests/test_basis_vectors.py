09dbedba010cdad47e052d8edfcddc8aad7061ec
Thomas Branfield <tbranfield@gmail.com>
2023-10-21 21:12:50 +0100

    Wrote a test for generating SrTiO3 basis vectors

--- tests/test_basis_vectors.py
+++ tests/test_basis_vectors.py
@@ -1,6 +1,9 @@
 """test_basis_vectors.py"""
 
 
+import numpy as np
+import pytest
+
 from crystal_visualizer.basis_vectors import BasisVectors
 from crystal_visualizer.cif_reader import CifReader
 
@@ -10,6 +13,7 @@ from crystal_visualizer.cif_reader import CifReader
 def test_generate_correct_basis_vectors_for_SrTiO3():
     cif_reader = CifReader("SrTiO3.cif")
     basis_vectors = BasisVectors(cif_reader.lattice_parameters)
-    print(basis_vectors.a)
-    print(basis_vectors.b)
-    print(basis_vectors.c)
+    cell_length = cif_reader.lattice_parameters.length_a
+    assert np.array_equal(basis_vectors.a, [cell_length, 0.0, 0.0])
+    assert np.array_equal(basis_vectors.b, [0, cell_length, 0.0])
+    assert np.array_equal(basis_vectors.c, [0, 0, cell_length])
