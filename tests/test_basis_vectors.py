6bf2ed0a82920b809e3bba0e50bfb428dcecd501
Thomas Branfield <tbranfield@gmail.com>
2023-10-21 21:24:55 +0100

    Added a test for the correct basis vectors for SiO2

--- tests/test_basis_vectors.py
+++ tests/test_basis_vectors.py
@@ -8,8 +8,7 @@ from crystal_visualizer.basis_vectors import BasisVectors
 from crystal_visualizer.cif_reader import CifReader
 
 
-
-@pytest.mark.current
+# Using SrTiO3 as has cubic cell with orthogonal axes of equal lengths
 def test_generate_correct_basis_vectors_for_SrTiO3():
     cif_reader = CifReader("SrTiO3.cif")
     basis_vectors = BasisVectors(cif_reader.lattice_parameters)
@@ -17,3 +16,17 @@ def test_generate_correct_basis_vectors_for_SrTiO3():
     assert np.array_equal(basis_vectors.a, [cell_length, 0.0, 0.0])
     assert np.array_equal(basis_vectors.b, [0, cell_length, 0.0])
     assert np.array_equal(basis_vectors.c, [0, 0, cell_length])
+
+
+# SiO2 has non-orthogonal axes
+# We calculate the correct expected values by hand
+@pytest.mark.current
+def test_generate_correct_basis_vectors_for_SiO2():
+    cif_reader = CifReader("SiO2.cif")
+    basis_vectors = BasisVectors(cif_reader.lattice_parameters)
+    expected_a = [4.9019, 0, 0]
+    expected_b = [-2.45095, 4.2452, 0]
+    expected_c = [0, 0, 5.3988]
+    assert np.allclose(basis_vectors.a, expected_a)
+    assert np.allclose(basis_vectors.b, expected_b)
+    assert np.allclose(basis_vectors.c, expected_c)
